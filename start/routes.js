8e6f922c031d38fc25324c96ac9108a760c9b2dc
Rafael Vicio <rafaelvicio@icloud.com>
2019-01-04 00:42:44 -0200

    Cadastro e login

--- start/routes.js
+++ start/routes.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +14,9 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use("Route");
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post("/register", "AuthController.register");
+Route.post("/authenticate", "AuthController.authenticate");
+
+Route.get("/app", "AppController.index").middleware(["auth"]);
