cfcfc51f357ca6f00dc05da1f46be5e2a8454c4d
Rafael Vicio <rafaelvicio@icloud.com>
2019-01-06 04:45:06 -0200

    Resgatar Cupom

--- app/Controllers/Http/CodeController.js
+++ app/Controllers/Http/CodeController.js
@@ -6,7 +6,11 @@
 
 const moment = require('moment');
 
+const User = use("App/Models/User");
 const Code = use("App/Models/Code");
+const UserCode = use("App/Models/UserCode");
+const History = use("App/Models/History");
+const Database = use('Database')
 
 /**
  * Resourceful controller for interacting with codes
@@ -20,43 +24,54 @@ class CodeController {
     auth
   }) {
 
-    const code = await Code.findOrFail(params.id);
+    let user;
 
-    var codeValidation = moment(code.date_validation);
-
-    if (!codeValidation.isAfter(moment())) {
-      console.log("Ja passou")
-      return
-    } else {
-      console.log("Ainda nao passou")
-    }
-
-    //Verificar se cupom ja chegou ao limite
-
-    //Verificar se ususario ja resgatou
-
-    // Criando Historico
     try {
-      const user = await auth.getUser()
-
-      const historyData = {
-        name: "Cupom resgatado",
-        description: `Cupom ${code.code}`,
-        type: "Cupom",
-        points: code.points,
-        user_id: user.id,
-        date_validity: moment().add(3, 'years')
-      }
-  
-      await History.create(historyData);
-
+      const authUser = await auth.getUser()
+      user = await User.findOrFail(authUser.id);
     } catch (error) {
       response.send('Missing or invalid jwt token')
+      return
     }
 
-    // await user.codes().attach(params.id)
+    const code = await Database.from("codes").where({
+      code: params.id
+    }).first()
 
-    // return user;
+    if (code.length == 0) {
+      response.send("Cupom nao encontrado")
+      return
+    }
+
+    if (!moment(code.date_validation).isAfter(moment())) {
+      response.send("Cupom ja vencido")
+      return
+    }
+
+    //Verificar se ususario ja resgatou
+    const userCode = await Database.from("user_codes").where({
+      user_id: user.id,
+      code_id: code.id
+    })
+
+    if (userCode.length != 0) {
+      response.send("Voce ja resgatou esse cupom")
+      return
+    }
+
+    //Verificar se cupom ja chegou ao limite de uso
+
+    const history = {
+      name: "Cupom resgatado",
+      description: `Cupom ${code.code}`,
+      type: "Cupom",
+      points: code.points,
+      user_id: user.id,
+      date_validity: moment().add(3, 'years')
+    }
+
+    await History.create(history);
+    await user.codes().attach(code.id)
   }
 
   /**
